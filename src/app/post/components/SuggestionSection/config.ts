26911849ecce62b0a9151f735fe0fa35178efbf7
Ilyas022 <ilya.shchetko@mail.ru>
2024-06-23 02:05:52 +0300

    feat: add suggestion section for post page

---
+++ src/app/post/components/SuggestionSection/config.ts
@@ -0,0 +1,25 @@
+import postImg from '@/assets/postImage.png'
+
+export const suggestions = [
+	{
+		img: postImg,
+		userName: 'John Doe',
+		date: 'Aug 23, 2021 ',
+		title: 'A UX Case Study Creating a Studious Environment for Students: ',
+		text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+	},
+	{
+		img: postImg,
+		userName: 'John Doe',
+		date: 'Aug 23, 2021 ',
+		title: 'A UX Case Study Creating a Studious Environment for Students: ',
+		text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+	},
+	{
+		img: postImg,
+		userName: 'John Doe',
+		date: 'Aug 23, 2021 ',
+		title: 'A UX Case Study Creating a Studious Environment for Students: ',
+		text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+	},
+]
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,10 +1,12 @@
 import PostSection from './components/PostSection'
+import SuggestionSection from './components/SuggestionSection'
 
 function Post() {
 	return (
 		<main>
 			<div className="container">
 				<PostSection />
+				<SuggestionSection />
 			</div>
 		</main>
 	)
