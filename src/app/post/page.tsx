e327c91f0d72fc76cdeb1246c0f67ad201bd3611
Ilyas022 <ilya.shchetko@mail.ru>
2024-06-23 01:23:29 +0300

    feat: add post section for blog page

---
+++ src/app/post/page.tsx
@@ -0,0 +1,13 @@
+import PostSection from './components/PostSection'
+
+function Post() {
+	return (
+		<main>
+			<div className="container">
+				<PostSection />
+			</div>
+		</main>
+	)
+}
+
+export default Post
