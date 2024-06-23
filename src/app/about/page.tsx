3444cf5caecca917421ee8f717b6a4c6665add79
Ilyas022 <ilya.shchetko@mail.ru>
2024-06-23 19:21:25 +0300

    feat: implement reason section and authors section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,16 @@
+import AuthorsSection from '@/components/AuthorsSection'
+
 import AboutUsSection from './components/AboutUsSection'
+import ReasonsSection from './components/ReasonsSection'
+import { authors } from './config'
 
 function About() {
 	return (
 		<main>
 			<div className="container">
 				<AboutUsSection />
+				<ReasonsSection />
+				<AuthorsSection authors={authors} />
 			</div>
 		</main>
 	)
---
+++ src/app/components/WeAreSection/config.tsx
@@ -0,0 +1,13 @@
+import CompanyLogo1 from '@/assets/icons/CompanyLogo1'
+import CompanyLogo2 from '@/assets/icons/CompanyLogo2'
+import CompanyLogo3 from '@/assets/icons/CompanyLogo3'
+import CompanyLogo4 from '@/assets/icons/CompanyLogo4'
+import CompanyLogo5 from '@/assets/icons/CompanyLogo5'
+
+export const companies = [
+	<CompanyLogo1 key={1} />,
+	<CompanyLogo2 key={2} />,
+	<CompanyLogo3 key={3} />,
+	<CompanyLogo4 key={4} />,
+	<CompanyLogo5 key={5} />,
+]
