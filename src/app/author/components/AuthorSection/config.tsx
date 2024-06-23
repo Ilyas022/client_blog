7ad4a623c81ca0ba3256c9abe63ee005eb95999b
Ilyas022 <ilya.shchetko@mail.ru>
2024-06-23 19:54:52 +0300

    feat: add author section for author page

---
+++ src/app/author/components/AuthorSection/config.tsx
@@ -0,0 +1,11 @@
+import FaceBookIcon from '@/assets/icons/FaceBookIcon'
+import InstagramIcon from '@/assets/icons/InstagramIcon'
+import LinkedInIcon from '@/assets/icons/LinkedInIcon'
+import TwitterIcon from '@/assets/icons/TwitterIcon'
+
+export const socialNetworks = [
+	<FaceBookIcon key={1} />,
+	<TwitterIcon key={2} />,
+	<InstagramIcon key={3} />,
+	<LinkedInIcon key={4} />,
+]
---
+++ src/app/author/page.tsx
@@ -0,0 +1,12 @@
+import AuthorSection from './components/AuthorSection'
+
+function Author() {
+	return (
+		<main>
+			<AuthorSection />
+			<div className="container" />
+		</main>
+	)
+}
+
+export default Author
